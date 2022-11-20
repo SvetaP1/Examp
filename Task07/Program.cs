1d56c0980a1a001623cdcba772da5a7cef9a89e2
Svetlana <s.ponikarova@gmail.com>
2022-11-20 20:23:34 +0300

    Task07 (последняя цифра 3-значного числа)

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿// Console.WriteLine("Введите трехзначное число");
+
+// Int32 nomber = Convert.ToInt32(Console.ReadLine());
+// Int32 result = nomber%10;
+
+// Console.WriteLine(result);
+
+Console.WriteLine("Введите трехзначное число");
+Int32 nomber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(nomber%10);
