45d7c9b8cbc40624fd75ea4b7ca367f5108aaeb2
Svetlana <s.ponikarova@gmail.com>
2022-11-20 18:59:36 +0300

    Task01 (квадрат числа)

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа");
+int nomber1 = Convert.ToInt32(Console.ReadLine());
+int nomber2 = Convert.ToInt32(Console.ReadLine());
+
+if (nomber1/nomber2 == nomber2)
+{
+    Console.WriteLine($"число {nomber1} является квадратом числа {nomber2}");
+}
+else
+{
+    Console.WriteLine($"число {nomber1} не является квадратом числа {nomber2}");
+}
