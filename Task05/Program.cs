f825ad6be5f2da33592ce6fb22f79f5ef51eb598
Svetlana <s.ponikarova@gmail.com>
2022-11-20 20:02:07 +0300

    Task05 (последовательность чисел от -N до N)

---
+++ Task05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число:");
+int nomber = Convert.ToInt32(Console.ReadLine());
+int result = nomber * (-1);
+
+while (result <= nomber)
+{
+    Console.Write(result);
+    result = result + 1;
+    Console.Write(",");
+}
+  
