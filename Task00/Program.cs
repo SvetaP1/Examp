aab7322155f8d4ee4d71f541601289254a5c6670
Svetlana <s.ponikarova@gmail.com>
2022-11-20 19:05:09 +0300

    Task00

---
+++ Task00/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу,
+// которая на вход принимает число
+// выдаёт его квадрат (число умноженное на само себя).
+
+// Например: 
+// 4 -> 16 
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Ведите целое число:");
+int nomber = Convert.ToInt32(Console.ReadLine());
+// int nomber1 = int.Parse(Console.ReadLine()); - альтернативный способ перевести строку в число.
+int square = nomber*nomber;
+Console.WriteLine($"Квадрат числа {nomber} = {square}.");
