7e7203918b699e7395af9297adc6c3f84c9cc0e5
Svetlana <s.ponikarova@gmail.com>
2022-12-12 00:11:13 +0300

    add new tasks

---
+++ Task51/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 51:
+// Задайте двумерный массив.
+// Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1,1) и т.д.)
+
+int[,] CraeteMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+int SumDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) sum += matrix[i, j];
+        }
+    }
+    return sum;
+}
+
+int[,] array2D = CraeteMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+int sumDiagonal = SumDiagonal(array2D);
+Console.WriteLine(sumDiagonal);
